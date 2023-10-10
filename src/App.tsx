c15b310f253c35f6e4e92c4e8b79c270bc440ca7
AnifInnab <Anif@innab.se>
2023-10-10 13:39:04 +0700

    rm useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Circle from "./Circle";
 import { useWakeLock } from "react-screen-wake-lock";
