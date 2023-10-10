42de0e6c61bdd543d408f50472fa99ad0b783fd8
AnifInnab <Anif@innab.se>
2023-10-10 13:07:56 +0700

    wl test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import Circle from "./Circle";
+import { useWakeLock } from "react-screen-wake-lock";
 
 const App = () => {
+  const { isSupported, released, request } = useWakeLock({
+    onRequest: () => alert("Screen Wake Lock: requested!"),
+    onError: () => alert("An error happened 💥"),
+    onRelease: () => alert("Screen Wake Lock: released!"),
+  });
+  console.log({ isSupported, released });
+  useEffect(() => {
+    requestWakeLock();
+  }, []);
+
+  const requestWakeLock = async () => {
+    if (!released) {
+      await request();
+    }
+  };
   return (
     <div className="app">
+      <p>{isSupported ? "WL Supported" : "WL not supported"}</p>
+      <p>{released ? "WL Released" : "WL not Released"}</p>
+
       <div className="row">
         <Circle />
         <Circle />
